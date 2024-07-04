6465074005227a7bee7e6bca20fd232124f183d5
nothing-stack <black3071@163.com>
2024-07-04 13:14:05 +0800

    1.增加消息队列，信号量。在线程间的简单通信和同步操作。

--- thread.cpp
+++ thread.cpp
@@ -4,8 +4,9 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <semaphore.h>
 //多线程中互斥锁与条件变量一起使用，保证同一时刻只有一个线程可以访问共享资源，来避免访问共享资源产生的竞态问题
-//wait wait_for都是阻塞性，前者一直等待条件满足，后者等待一定时间。
+//wait,wait_for都是阻塞性，前者一直等待条件满足，后者等待一定时间。
 /*使用注意点：
 1.正确初始化条件变量。
 2.在持有互斥锁的情况下才能调用条件变量的等待函数，避免竞态情况。
@@ -16,7 +17,10 @@
 using namespace std;
 std::mutex mtx;
 std::condition_variable cv;
+//消费队列
 std::queue<int> products;
+//处理消息队列
+std::queue<int> Messages;
 
 void producerThread(int id)
 {
@@ -50,7 +54,7 @@ void consumerThread(int id)
         if(cv.wait_for(lock,std::chrono::seconds(1),[]{return !products.empty();}))
         {
             int product = products.front();
-            products.pop();
+            products.pop();//进行消费
             cout<<"void consumerThread() ->"<<this_thread::get_id()<<":"<<product<<endl;
             lock.unlock();
             cv.notify_all();
@@ -59,23 +63,88 @@ void consumerThread(int id)
     }
 }
 
-void testThread1()
+
+//消息队列进行数据传递
+bool stop  = false;
+
+void sendThread1()
 {
-    std::unique_lock<std::mutex> lock(mtx);
-    for(int i = 0;i < 10;i++)
+    for(int i = 0; i < 10; i++)
     {
-        Sleep(1000);
-        cout<<"testThread1 :"<<i<<endl;
+        std::unique_lock<std::mutex> lock(mtx);
+        Messages.push(i);
+        cout<<"send date :"<<i<<endl;
+        cv.notify_one();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(mtx); //防止干扰
+        stop = true;
+    }
+
+    //消息发送完，唤醒所有等待接受者
+    cv.notify_all();
+    
+}
+
+void recvThread2()
+{
+    while(1){
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock,[]{return !Messages.empty() || stop;});
+        if(stop && Messages.empty()){
+            break; //停止处理
+        }
+
+        int msg = Messages.front();
+        Messages.pop();
+        lock.unlock();
+        cout<<"received msg:"<< msg <<endl;
     }
 }
 
-void testThread2()
+/*
+线程间的通信方式：
+1.利用共享内存实现通信，利用线程锁处理同步问题
+2.消息队列和管道
+3.简单交互可使用条件变量
+4.信号量
+
+注意：
+线程同步：确保线程之间能有序访问共享资源，防止数据竞争和确保数据一致性。
+线程通信：线程间相互交换信息和数据，来协调各自行为。
+*/
+
+
+//信号量简单使用和理解
+int shared_resource = 0;
+sem_t semaphore;
+
+void increase_sem_thread()
 {
-    std::unique_lock<std::mutex> lock(mtx);
-    for(int i = 0;i < 10;i++)
+    while(1)
     {
-        Sleep(1000);
-        cout<<"testThread2 :"<<i<<endl;
+        sem_wait(&semaphore);
+        shared_resource++;
+        cout<<"the increase resource:"<<shared_resource<<endl;
+        sem_post(&semaphore);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
+void reduce_sem_thread()
+{
+    while(1)
+    {
+        if(shared_resource)
+        {
+            sem_wait(&semaphore);
+            shared_resource--;
+            cout<<"the reduce resource:"<<shared_resource<<endl;
+            sem_post(&semaphore);
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
@@ -88,20 +157,25 @@ int main(int argc,char *argv[])
  
     std::srand(std::time(0));
 
-    /*std::thread Thread1(testThread1);
-    std::thread Thread2(testThread2);
-    
-    Thread1.join();
-    Thread2.join();*/
-
-    std::thread producer(producerThread,1),producerx(producerThread,2);
-    std::thread consumer(consumerThread,1),consumerx(consumerThread,2);
+    /*std::thread producer(producerThread,1);
+    std::thread consumer(consumerThread,1);
 
     producer.detach();
-    producerx.detach();
+    consumer.detach();*/
+  
+    /*std::thread sendThread(sendThread1);
+    std::thread recvThread(recvThread2);
+
+    sendThread.detach();
+    recvThread.detach();*/
+
+    std::thread producer(increase_sem_thread);
+    std::thread consumer(reduce_sem_thread);
+
+    producer.detach();
     consumer.detach();
-    consumerx.detach();
 
     while(1);
+
     return 0;
 }
