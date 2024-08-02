a9e0b62a690b00b3cf2614b0cccd17c5fc5d9328
nothing-stack <black3071@163.com>
2024-08-02 15:44:31 +0800

    删除无用信息

--- process.cpp
+++ process.cpp
@@ -12,35 +12,12 @@
 *保留段：不可访问地址。（NULL）
 */
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-    cout<<"the test fork process start"<<endl;
-    
-    STARTUPINFO si = {sizeof(si)};
-    PROCESS_INFORMATION pi;
-    si.cb = sizeof(si);
-    ZeroMemory(&pi, sizeof(pi));
- 
-    // 这里的"C:\\path\\to\\program.exe"是新进程要运行的程序路径
-    TCHAR lpCommandLine[] = TEXT("test.exe");
-    int ret = CreateProcess(NULL,lpCommandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
-    if (ret > 0 ) {
-        while(1)
-        {
-            std::cout << "子进程test" << std::endl;
-        }
-
-        WaitForSingleObject(pi.hProcess, INFINITE); //等待进程退出，回收资源
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
-    }
-    else {
-        std::cerr << "创建进程失败." << ret << std::endl;
-    }
-
     return 0;
 }
+
+//
